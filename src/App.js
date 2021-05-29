5e3068c1c660d7455560cdd88172782bc2661e61
ines <ines@1D-INES.DC-CAPES.COM>
2021-05-29 11:10:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import imgSrc from './imgSrc.png';
+//import './App.css';
+import './Style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    < div className="App">
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">INES ZEKRI</h1>
+      <br />
+          
+          <img src={imgSrc} className="App-logo" alt="logo1" />
+          <br /> <br /> <br /> 
+          <img src="/imgPublic.png" className="App-img"alt="image"/>
+         
+
+    <br />    <br />    <br />    <br />
+    <video width={850} height={250} controls>
+      <source src="myVideo.mp4" type="video/mp4"  />
+    </video>
+  </div>
+  </div>
+);
 }
+
 
 export default App;
